0c86b6143b16b0966ace1aa55473c040da56cf2d
David Wildasin <dragonmaus@posteo.net>
2022-01-05 23:23:28 -0500

    Initial commit

---
+++ src/bin/fixpaths.rs
@@ -0,0 +1,56 @@
+use getopt::Opt;
+use realpath::realpaths;
+use std::{env, io};
+
+program::main!("fixpaths");
+
+fn print_usage(program_name: &str) {
+    println!("Usage: {} [-h] [-e VAR] [string ...]", program_name);
+    println!("  -e VAR  operate on the value of environment variable VAR");
+    println!();
+    println!("  -h      display this help");
+}
+
+fn program(name: &str) -> program::Result {
+    let mut args = program::args();
+    let mut opts = getopt::Parser::new(&args, "e:h");
+
+    let mut pathss: Vec<String> = Vec::new();
+    loop {
+        match opts.next().transpose()? {
+            None => break,
+            Some(opt) => match opt {
+                Opt('e', Some(key)) => match env::var_os(&key) {
+                    None => {
+                        return Err(io::Error::new(
+                            io::ErrorKind::InvalidData,
+                            format!("{} not set", key),
+                        )
+                        .into());
+                    }
+                    Some(value) => pathss.push(value.to_string_lossy().into_owned()),
+                },
+                Opt('h', None) => {
+                    print_usage(name);
+                    return Ok(0);
+                }
+                _ => unreachable!(),
+            },
+        }
+    }
+
+    pathss.append(args.split_off(opts.index()).as_mut());
+
+    if pathss.is_empty() {
+        match env::var_os("PATH") {
+            None => return Err(io::Error::new(io::ErrorKind::InvalidData, "PATH not set").into()),
+            Some(value) => pathss.push(value.to_string_lossy().into_owned()),
+        }
+    }
+
+    for paths in pathss {
+        println!("{}", realpaths(&paths)?.to_string_lossy());
+    }
+
+    Ok(0)
+}
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use getopt::Opt;
+use realpath::realpath;
+
+program::main!("realpath");
+
+fn print_usage(program_name: &str) {
+    println!("Usage: {} [-0h] path [path ...]", program_name);
+    println!("  -0   terminate output lines with NUL, not newline");
+    println!();
+    println!("  -h   display this help");
+}
+
+fn program(name: &str) -> program::Result {
+    let mut args = program::args();
+    let mut opts = getopt::Parser::new(&args, "0h");
+
+    let mut eol = '\n';
+
+    loop {
+        match opts.next().transpose()? {
+            None => break,
+            Some(opt) => match opt {
+                Opt('0', None) => eol = '\0',
+                Opt('h', None) => {
+                    print_usage(name);
+                    return Ok(0);
+                }
+                _ => unreachable!(),
+            },
+        }
+    }
+
+    let mut args = args.split_off(opts.index());
+
+    if args.is_empty() {
+        args.push(String::from("."));
+    }
+
+    for arg in args {
+        print!("{}{}", realpath(&arg)?.to_string_lossy(), eol);
+    }
+
+    Ok(0)
+}
